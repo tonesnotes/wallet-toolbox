c22139cdf5d0a5f1b507331d1fe522e2111063e6
Brayden Langley <brayden167@gmail.com>
2026-02-04 11:28:56 -0800

    Update src/WalletPermissionsManager.ts

--- src/WalletPermissionsManager.ts
+++ src/WalletPermissionsManager.ts
@@ -425,7 +425,9 @@ export interface PermissionsManagerConfig {
   differentiatePrivilegedOperations?: boolean
 
   /**
-   * If a counterparty and protocol name is in this list, it is automatically granted permission without prompting the user.
+   * An allowlist mapping counterparty identity public keys (hex)
+   * to protocol names that are automatically permitted
+   * without prompting the user.
    */
   whitelistedCounterparties?: { [counterparty: PubKeyHex]: string[] }
 }
